c97235a0d75ea5c0574f573f49113ba380a6db08
nikhilcodes0 <nk7488577732@gmail.com>
2024-10-17 02:16:26 +0530

    Added Navbar and chart on homepage

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,75 @@
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { Button } from "@/components/ui/button";
+import logo from "../assets/logo-leaf.svg";
+
+function Navbar() {
+  return (
+    <div className="bg-[rgba(255,_255,_255,_0.05)] border-[1.3px]  border-[rgba(255,255,255,0.05)] rounded-lg w-[90%] mx-auto my-6 px-3 py-4 ">
+      <div className="flex justify-between">
+        <div className="flex gap-2 items-center">
+          <p className="font-[Mynerve] text-white">AgroCult</p>
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="text-white">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button size="icon"><HamburgerMenuIcon/></Button>
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle className="text-white text-left">
+                  Available Links!
+                </SheetTitle>
+                <SheetDescription className="text-white text-left opacity-50">
+                  Click on any of the links below to get started!
+                </SheetDescription>
+              </SheetHeader>
+              <div className="p-4 pb-0 mt-3">
+                <div className="flex items-center justify-center gap-4 flex-col">
+                  <SheetClose className="w-full">
+                    <Button
+                      variant="default"
+                      size="icon"
+                      className="h-12 w-full shrink-0 rounded-full"
+                    >
+                      Home
+                    </Button>{" "}
+                  </SheetClose>
+                  <SheetClose className="w-full">
+                    <Button
+                      variant="secondary"
+                      size="icon"
+                      className="h-12 w-full shrink-0 rounded-full"
+                    >
+                      About US
+                    </Button>
+                  </SheetClose>
+                  <SheetClose className="w-full">
+                    <Button
+                      variant="secondary"
+                      size="icon"
+                      className="h-12 w-full shrink-0 rounded-full"
+                    >
+                      Stats
+                    </Button>
+                  </SheetClose>
+                </div>
+              </div>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,93 @@
+import Navbar from "@/components/Navbar";
+import { Area, AreaChart, CartesianGrid, XAxis } from "recharts";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+
+const chartData = [
+  { month: "January", desktop: 186 },
+  { month: "February", desktop: 305 },
+  { month: "March", desktop: 237 },
+  { month: "April", desktop: 73 },
+  { month: "May", desktop: 209 },
+  { month: "June", desktop: 214 },
+  { month: "July", desktop: 186 },
+  { month: "August", desktop: 305 },
+  { month: "September", desktop: 237 },
+  { month: "October", desktop: 73 },
+  { month: "November", desktop: 209 },
+  { month: "December", desktop: 214 },
+];
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "#5d9763",
+  },
+} satisfies ChartConfig;
+
+function Home() {
+
+    const latestData = chartData[chartData.length - 1];
+
+  return (
+    <>
+      <Navbar />
+      <Card className="w-4/5 mx-auto bg-[#eaffe7]">
+        <CardHeader>
+          <CardTitle>Temperature</CardTitle>
+          <CardDescription className="text-black">
+            <p className="font-semibold text-2xl">{latestData.desktop}°C</p>
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <ChartContainer config={chartConfig}>
+            <AreaChart
+              accessibilityLayer
+              data={chartData}
+              margin={{
+                left: 12,
+                right: 12,
+              }}
+            >
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey="month"
+                tickLine={false}
+                axisLine={false}
+                tickMargin={8}
+                tickFormatter={(value) => value.slice(0, 3)}
+              />
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent indicator="line" />}
+                
+              />
+              <Area
+                dataKey="desktop"
+                type="natural"
+                fill="var(--color-desktop)"
+                fillOpacity={0.4}
+                stroke="var(--color-desktop)"
+              />
+            </AreaChart>
+          </ChartContainer>
+        </CardContent>
+        
+      </Card>
+
+    </>
+  );
+}
+
+export default Home;
