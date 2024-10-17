fd288b24405ebbb0e12ec820d1e2a310e6607f04
Harsh Pratap Singh <74965077+HPSingh7146@users.noreply.github.com>
2024-10-18 04:00:06 +0530

    made about page responsive

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -11,47 +11,50 @@ function About() {
   return (
     <>
       <Navbar />
-      <div className="text-[#dcffd7] p-8">
-        <div>
-          <p className="text-5xl font-semibold">About Us</p>
-          <div className="mt-8">
-            <img
-              src={leafBg}
-              alt=""
-              className="absolute -z-10 mx-[4rem] brightness-75 w-[55%] top-64"
-            />
-            <img src={greenLeaf} alt="Leaf" className="mx-auto w-[85%]" />
+      <div className="text-[#dcffd7] py-8 w-[90%] mx-auto">
+        <div className="lg:flex lg:gap-14 w-[100%] justify-between">
+          <div>
+            <p className="text-5xl font-semibold lg:hidden">About Us</p>
+            <div className="mt-8">
+              <img
+                src={leafBg}
+                alt=""
+                className="absolute -z-10 mx-[4rem]  brightness-75 w-[55%] md:w-[40%] md:mx-[11rem] top-64 lg:mx-[2rem] lg:w-[23%] lg:top-52"
+              />
+              <img src={greenLeaf} alt="Leaf" className="mx-auto w-[85%] md:w-[60%] lg:w-[31rem]" />
+            </div>
+            <div className="lg:w-[60%] lg:left-[-9%]  md:w-[40%] bg-[#24ff02] w-[30%] h-[485.93px] left-[30%] top-[20%] opacity-[0.35] filter blur-[84.03px] rotate-[35.24deg] absolute -z-20"></div>
+            <div className="lg:left-[12%] lg:bottom-[23%] md:left-[27%] md:bottom-[23%] bg-[#fcff45] w-20 h-20 rounded-full flex items-center justify-center p-4 relative left-20 bottom-24">
+              <p className="text-black font-semibold">Monstera</p>
+            </div>
           </div>
-          <div className="bg-[#24ff02] w-[30%] h-[485.93px] left-[30%] top-[20%] opacity-[0.35] filter blur-[84.03px] rotate-[35.24deg] absolute -z-20"></div>
-          <div className="bg-[#fcff45] w-20 h-20 rounded-full flex items-center justify-center p-4 relative left-20 bottom-24">
-            <p className="text-black font-semibold">Monstera</p>
+          <div className="lg:w-[60%]">
+            <p className="text-7xl mt-5 mb-20 font-semibold hidden lg:block">About Us</p>
+            <p className="text-justify font-medium tracking-wider text-white text-lg paragraph ">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod totam
+              animi fugit fugiat laboriosam ad ab ut sit odio! Error, molestias
+              illum debitis at nesciunt quia sunt rem qui accusantium, incidunt
+              vel! In reiciendis tempora sit quam harum velit magnam nobis
+              deserunt veniam libero vel alias atque molestiae cupiditate amet
+              quia recusandae nostrum labore dolor at, error rerum laboriosam
+              repudiandae corporis. Rerum, omnis deserunt possimus ullam atque
+              error dicta sequi! <br />
+              <br />
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi,
+              corrupti quos ratione totam necessitatibus laudantium quidem
+              provident exercitationem accusamus sint et impedit accusantium
+              cupiditate nulla voluptatem praesentium. Fugit adipisci harum quis
+              earum mollitia veniam atque hic porro autem repellendus suscipit
+              possimus quidem nesciunt, voluptatum cumque, odit aliquam.
+              Temporibus provident repellendus, nam at doloribus fugiat aperiam
+              maiores nobis iste cupiditate, impedit rem aspernatur possimus
+              suscipit. Ab saepe laborum quaerat voluptas excepturi!
+            </p>
           </div>
-        </div>
-        <div>
-          <p className="text-justify font-medium tracking-wider text-white text-lg paragraph">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod totam
-            animi fugit fugiat laboriosam ad ab ut sit odio! Error, molestias
-            illum debitis at nesciunt quia sunt rem qui accusantium, incidunt
-            vel! In reiciendis tempora sit quam harum velit magnam nobis
-            deserunt veniam libero vel alias atque molestiae cupiditate amet
-            quia recusandae nostrum labore dolor at, error rerum laboriosam
-            repudiandae corporis. Rerum, omnis deserunt possimus ullam atque
-            error dicta sequi! <br />
-            <br />
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi,
-            corrupti quos ratione totam necessitatibus laudantium quidem
-            provident exercitationem accusamus sint et impedit accusantium
-            cupiditate nulla voluptatem praesentium. Fugit adipisci harum quis
-            earum mollitia veniam atque hic porro autem repellendus suscipit
-            possimus quidem nesciunt, voluptatum cumque, odit aliquam.
-            Temporibus provident repellendus, nam at doloribus fugiat aperiam
-            maiores nobis iste cupiditate, impedit rem aspernatur possimus
-            suscipit. Ab saepe laborum quaerat voluptas excepturi!
-          </p>
         </div>
         <Separator className="my-10 w-[95%] mx-auto opacity-100 bg-[#dcffd71a] p-[1px]" />
 
-        <div className="bg-[#24513b] p-4 flex flex-col gap-3 rounded-lg w-[100%]">
+        <div className="bg-[#24513b] p-4 grid md:grid-cols-2 gap-3 rounded-lg w-[100%]">
           <div className="flex gap-2 w-full">
             <img src={img1} alt="" className="w-[50%] rounded-md" />
             <img src={img2} alt="" className="w-[50%] rounded-md"/>
