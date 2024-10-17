8b92c0470e67497694a521289cf3be741995d6eb
Harsh Pratap Singh <74965077+HPSingh7146@users.noreply.github.com>
2024-10-18 05:06:11 +0530

    added footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,38 @@
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
 
+import { Button } from "@/components/ui/button"
 
 function Footer() {
-  return (
-    <div className="text-black text-2xl">Made by </div>
-  )
+return (
+  <>
+      <HoverCard>
+    <HoverCardTrigger asChild>
+      <Button variant="link">@nextjs</Button>
+    </HoverCardTrigger>
+    <HoverCardContent className="w-80">
+      <div className="flex justify-between space-x-4">
+        <div className="space-y-1">
+          <h4 className="text-sm font-semibold">@nextjs</h4>
+          <p className="text-sm">
+            The React Framework  created and maintained by @vercel.
+          </p>
+          <div className="flex items-center pt-2">
+            {/* <CalendarIcon className="mr-2 h-4 w-4 opacity-70" />{" "} */}
+            <span className="text-xs text-muted-foreground">
+              Joined December 2021
+            </span>
+          </div>
+        </div>
+      </div>
+    </HoverCardContent>
+  </HoverCard>
+  
+  </>
+)
 }
 
 export default Footer
