5a86024cb28b5669e0ee402088d0c20350d9a2d4
nikhilcodes0 <nk7488577732@gmail.com>
2024-11-07 20:54:12 +0530

    changed the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,44 +17,28 @@ function Footer() {
       </div>
       <HoverCard>
         <HoverCardTrigger asChild>
-          <Button variant="link" className="text-white underline text-lg">Nikhil</Button>
+          <Button variant="link" className="text-white underline text-lg">KJC Tech Farmers</Button>
         </HoverCardTrigger>
         <HoverCardContent className="w-80">
           <div className="flex justify-between space-x-4">
             <div className="space-y-1">
-              <h4 className="text-sm font-semibold">NikhilCodes0</h4>
+              <h4 className="text-sm font-semibold">The TEAM</h4>
               <p className="text-sm">
-                Want to know more about me? Check out my socials!
+                Want to know more about us? Check out our Github!
               </p>
               <div className="flex items-center pt-2">
                 {/* <CalendarIcon className="mr-2 h-4 w-4 opacity-70" />{" "} */}
                 <span className="text-xs text-blue-500">
-                  <a href="https://github.com/nikhilcodes0">Github</a>
+                  <a href="https://github.com/nikhilcodes0" target="_blank">Nikhil</a>
                 </span>
                 <span className="text-xs text-blue-500 ml-4">
-                  <a href="https://x.com/nikhil__codes">Twitter</a>
+                  <a href="https://github.com/some-1hing" target="_blank">Yash Prakash</a>
                 </span>
-              </div>
-            </div>
-          </div>
-        </HoverCardContent>
-      </HoverCard>
-      <span className="text-white">&</span>
-      <HoverCard>
-        <HoverCardTrigger asChild>
-          <Button variant="link" className="text-white underline text-lg">Harsh</Button>
-        </HoverCardTrigger>
-        <HoverCardContent className="w-80">
-          <div className="flex justify-between space-x-4">
-            <div className="space-y-1">
-              <h4 className="text-sm font-semibold">Harsh Pratap Singh</h4>
-              <p className="text-sm">
-                Want to know more about me? Check out my socials!
-              </p>
-              <div className="flex items-center pt-2">
-                {/* <CalendarIcon className="mr-2 h-4 w-4 opacity-70" />{" "} */}
-                <span className="text-xs text-blue-500">
-                  <a href="https://github.com/HPSingh7146">Github</a>
+                <span className="text-xs text-blue-500 ml-4">
+                  <a href="https://github.com/hpsingh7146" target="_blank">Harsh Pratap</a>
+                </span>
+                <span className="text-xs text-blue-500 ml-4">
+                  <a href="https://github.com/R3tr0gh057" target="_blank">Joe Sanjo</a>
                 </span>
               </div>
             </div>
